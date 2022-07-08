b37c00f31e7d21065b74cebda21eda8b4059ae40
c3c1l1a <cesswangumukima@gmail.com>
2022-07-08 21:28:35 +0800

    Clean code a little

--- src/components/updateState.js
+++ src/components/updateState.js
@@ -2,10 +2,15 @@
 import operate from './operate';
 
 export default function updateState(key, state, setState){
-	const operations = ['+', '-', '%', 'x', '÷'];
-	const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 	const { operand1, operand2, operation, result } = state;
 
+	const groupKey = () => {
+		if (['+', '-', '%', 'x', '÷'].includes(key)) return 'operation';
+		if ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(key)) return 'number';
+		if (key === '=') return '=';
+		if (key === 'AC') return 'AC';
+	}
+	
 	const doOperation = () => {
 		const result = operate(operand1, operand2, operation);
 		setState({ 
@@ -16,33 +21,38 @@ export default function updateState(key, state, setState){
 		});
 	}
 
-	if (operations.includes(key)) {
-	  if ((operation !== null && operand2 === null) || operation === null) {
-	    setState({ ...state, operation: key });
-	  }
-	} else if (numbers.includes(key)) {
-	  if (operation === null) {
-	    if (operand1 === null || result === operand1) setState({ ...state, operand1: key });
-	    else setState({ ...state, operand1: parseInt(`${operand1}${key}`, 10) });
-	  } else if (operand2 === null) setState({ ...state, operand2: key });
-	  else setState({ ...state, operand2: parseInt(`${operand2}${key}`, 10) });
-	} else if (key === '=') {
-	  const hasOperand1 = operand1 !== null;
-	  const hasOperand2 = operand2 !== null;
-	  const hasOperation = operation !== null;
-	  if (hasOperand1 && hasOperand2 && hasOperation) doOperation();
-	} else if (key === 'AC') {
-	  if (operation === null && operand1 !== null) {
-	    let newOperand1 = `${operand1}`.slice(0, -1);
-	    newOperand1 = newOperand1.length > 0 ? newOperand1 : 0;
-	    newOperand1 = parseInt(newOperand1, 10);
-	    setState({...state, operand1: newOperand1 });
-	  } else if (operation !== null && operand2 !== null) {
-	    let newOperand2 = `${operand2}`.slice(0, -1);
-	    newOperand2 = newOperand2.length > 0 ? newOperand2 : 0;
-	    newOperand2 = parseInt(newOperand2, 10);
-	    newOperand2 = newOperand2 !== 0 ? newOperand2 : null;
-	    setState({ ...state, operand2: newOperand2 });
-	  }
+	const sliceDisplayNum = (num) => {
+		let newNum = `${num}`.slice(0, -1);
+	  newNum = newNum.length > 0 ? newNum : 0;
+	  newNum = parseInt(newNum, 10);
+	  return newNum;
+	}
+
+	switch(groupKey()){
+		case 'operation':
+			if ((operation !== null && operand2 === null) || operation === null) {
+			  setState({ ...state, operation: key });
+			}
+			break;
+		case 'number':
+			if (operation === null) {
+			  if (operand1 === null || result === operand1) setState({ ...state, operand1: key });
+			  else setState({ ...state, operand1: parseInt(`${operand1}${key}`, 10) });
+			} else if (operand2 === null) setState({ ...state, operand2: key });
+			else setState({ ...state, operand2: parseInt(`${operand2}${key}`, 10) });
+			break;
+		case '=': 
+			if (operand1 && operand2 && operation) doOperation();
+			break;
+		case 'AC':
+			if (operation === null && operand1 !== null) {
+			  let newOperand1 = sliceDisplayNum(operand1);
+			  setState({...state, operand1: newOperand1 });
+			} else if (operation !== null && operand2 !== null) {
+			  let newOperand2 = `${operand2}`.slice(0, -1);
+			  newOperand2 = sliceDisplayNum(operand2);
+			  newOperand2 = newOperand2 !== 0 ? newOperand2 : null;
+			  setState({ ...state, operand2: newOperand2 });
+			}
 	}
 }
