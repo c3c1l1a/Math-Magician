a8fbe23c1539ad0078dfe00a001bc97a1b814db3
c3c1l1a <cesswangumukima@gmail.com>
2022-07-14 21:44:42 +0800

    Add style for quote page

--- src/components/pages/Quote.js
+++ src/components/pages/Quote.js
@@ -1,5 +1,10 @@
+import '../css/quote.css';
+
 const Qoute = () => (
-  <div>Qoute</div>
+  <div className="quote">
+    <q className="body">Mathematics is not about numbers, computations or algorithms; it is about undestanding</q>
+    <q className="attribution">-Willian P. Thurston-</q>
+  </div>
 );
 
 export default Qoute;
